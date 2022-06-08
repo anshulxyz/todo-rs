8f6b566d4cb3fb8484f9b2b95bf45b50849fc33a
Anshul Chauhan <anshulchauhan@outlook.com>
2022-06-08 15:16:53 +0000

    Add cursive, remove tui-rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use cursive::Cursive;
+use cursive::views::{Dialog, TextView};
+
 fn main() {
-    println!("Hello, world!");
+    // initiate the cursive object
+    let mut siv = cursive::default();
+
+    // configure the cursive object
+    siv.add_global_callback('q', |s| s.quit());
+    siv.add_layer(
+        Dialog::text("text")
+            .title("title")
+            .button("Next", show_next),
+    );
+
+    // run the cursive object
+    siv.run()
+}
+
+fn show_next(s: &mut Cursive) {
+    s.pop_layer();
+    s.add_layer(
+        Dialog::text("Did you do the thang?!")
+        .title("Question")
+        .button("Yes", |s| ())
+        .button("No", |s| ())
+        .button("Quit", |s| s.quit())
+    )
 }
