9bc58a3e39356155796834406a97922f5e67d9b0
Anshul Chauhan <anshulchauhan@outlook.com>
2022-06-09 12:11:46 +0530

    Add local migration/entity to dependencies, run migrator in Main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    
+use std::error::Error;
+
+use migration::{Migrator, MigratorTrait};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let connection = sea_orm::Database::connect("sqlite://tasks.db").await?;
+    Migrator::up(&connection, None).await?;
+    Ok(())
 }
