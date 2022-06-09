019738c69e36cfbff577b974406077dc9d504285
Anshul Chauhan <anshulchauhan@outlook.com>
2022-06-09 14:10:18 +0000

    Add example of adding data into database, and UUI and DateTime

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
-use std::error::Error;
-
 use entity::task;
+use std::error::Error;
+use uuid::Uuid;
+
+use chrono::Utc;
 
 use migration::{Migrator, MigratorTrait};
-use sea_orm::EntityTrait;
+use sea_orm::{ActiveModelTrait, ActiveValue::NotSet, EntityTrait, InsertResult, Set};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let connection = sea_orm::Database::connect("sqlite://tasks.db?mode=rwc").await?;
-    Migrator::up(&connection, None).await?;
+    let db = sea_orm::Database::connect("sqlite://tasks.db?mode=rwc").await?;
+    Migrator::up(&db, None).await?;
+
+    // insert a task in data
+    let todo = task::ActiveModel {
+        id: Set(Uuid::new_v4().to_string()),
+        title: Set("Title".to_string()),
+        is_done: Set(0),
+        created_at: Set(Utc::now().naive_utc().to_string()),
+        text: NotSet,
+        finished_at: NotSet,
+        due_at: NotSet,
+    };
+
+    let todo = todo.insert(&db).await?;
+    println!("1. {:?}", todo);
 
     // fetch an item from database
-    let todo: task::Model = task::Entity::find().one(&connection).await.expect("Error fetching verse").unwrap();
-
-    println!("{:?}", todo);
+    let todo: task::Model = task::Entity::find()
+        .one(&db)
+        .await
+        .expect("Error fetching verse")
+        .unwrap();
+    println!("2. {:?}", todo);
 
     Ok(())
 }
