dea101b29cc2ccbab31426afa2284204dfdf3b45
Anshul Chauhan <anshulchauhan@outlook.com>
2022-06-09 13:02:32 +0530

    Update main, fetch item from data
    For Entity remove `mod.rs` since `lib.rs` is working for same purpose

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use std::error::Error;
 
+use entity::task;
+
 use migration::{Migrator, MigratorTrait};
+use sea_orm::EntityTrait;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let connection = sea_orm::Database::connect("sqlite://tasks.db?mode=rwc").await?;
     Migrator::up(&connection, None).await?;
+
+    // fetch an item from database
+    let todo: task::Model = task::Entity::find().one(&connection).await.expect("Error fetching verse").unwrap();
+
+    println!("{:?}", todo);
+
     Ok(())
 }
