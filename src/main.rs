f8b9be9be6b2f67f78b4739dedab04ace33591f4
Anshul Chauhan <anshulchauhan@outlook.com>
2022-06-10 19:48:27 +0000

    Fetch all undone tasks and display it in ListView

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use cursive::views::{ListView, TextView};
 use entity::task;
 use std::error::Error;
 use uuid::Uuid;
@@ -5,33 +6,33 @@ use uuid::Uuid;
 use chrono::Utc;
 
 use migration::{Migrator, MigratorTrait};
-use sea_orm::{ActiveModelTrait, ActiveValue::NotSet, EntityTrait, Set};
+use sea_orm::{ActiveModelTrait, ActiveValue::NotSet, ColumnTrait, EntityTrait, QueryFilter, Set};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let db = sea_orm::Database::connect("sqlite://tasks.db?mode=rwc").await?;
     Migrator::up(&db, None).await?;
 
-    // insert a task in data
-    let todo = task::ActiveModel {
-        id: Set(Uuid::new_v4().to_string()),
-        title: Set("Title".to_string()),
-        is_done: Set(0),
-        created_at: Set(Utc::now().naive_utc().to_string()),
-        text: NotSet,
-        finished_at: NotSet,
-        due_at: NotSet,
-    };
-
-    let todo = todo.insert(&db).await?;
-    println!("INSERT {:?}", todo);
-
-    // fetch an item from database
-    let todos = task::Entity::find()
+    let all_undone_tasks = task::Entity::find()
+        .filter(task::Column::IsDone.eq(0))
         .all(&db)
         .await
         .expect("Error fetching verse");
-    println!("FETCH {:?}", todos);
+    println!("{:?}", all_undone_tasks);
+
+    let mut list = ListView::new();
+
+    for (index, todo) in all_undone_tasks.iter().enumerate() {
+        let child_view = TextView::new(&todo.title);
+        list.add_child(&index.to_string(), child_view);
+    }
+
+    let mut siv = cursive::default();
+    siv.add_global_callback('q', |s| s.quit());
+
+    siv.add_layer(list);
+
+    siv.run();
 
     Ok(())
 }
