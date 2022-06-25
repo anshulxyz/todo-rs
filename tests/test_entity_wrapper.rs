df6e0e768ccbc8494233bc3f50d4a020463d1eea
Anshul Chauhan <anshulchauhan@outlook.com>
2022-06-25 14:00:07 +0000

    Update `test_create_task()`
    - Use `count()` to check number of entries before and after insertions

--- tests/test_entity_wrapper.rs
+++ tests/test_entity_wrapper.rs
@@ -1,5 +1,7 @@
 /// Tests for wrappers around entity for ease of operation
+use sea_orm::{EntityTrait, PaginatorTrait};
 use todo_rs::create_task;
+use entity::task;
 
 mod common;
 use common::get_db_conn;
@@ -9,7 +11,11 @@ use migration::DbErr;
 async fn test_create_task() -> Result<(), DbErr> {
     let task_title = "Task Title";
     let db = get_db_conn().await;
+    let count = task::Entity::find().count(&db).await.unwrap_or(0);
+    assert_eq!(0, count);
     let todo = create_task(&db, task_title).await.unwrap();
     assert_eq!(task_title, todo.title);
+    let count = task::Entity::find().count(&db).await.unwrap_or(0);
+    assert_eq!(1, count);
     Ok(())
 }
