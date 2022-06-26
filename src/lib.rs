7cc7fcc3106ba798e3bb027b072eacc1d94256c3
Anshul Chauhan <anshulchauhan@outlook.com>
2022-06-26 09:12:01 +0000

    Add function get_all_undone_tasks
    Also add test to get all undone tasks from a list of three tasks where
    one of them is marked done

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use chrono::{Local, SubsecRound};
 use entity::task;
-use sea_orm::{ActiveModelTrait, DbConn, EntityTrait, Set};
+use migration::DbErr;
+use sea_orm::{ActiveModelTrait, ColumnTrait, DbConn, EntityTrait, QueryFilter, Set};
 use uuid::Uuid;
 
-pub async fn create_task(db: &DbConn, title: &str) -> Result<task::Model, migration::DbErr> {
+pub async fn create_task(db: &DbConn, title: &str) -> Result<task::Model, DbErr> {
     let todo = task::ActiveModel {
         id: Set(Uuid::new_v4().to_owned().to_string()),
         title: Set(title.to_string()),
@@ -21,10 +22,18 @@ pub async fn update_task_is_done(
     db: &DbConn,
     task_id: String,
     is_done: bool,
-) -> Result<task::Model, migration::DbErr> {
+) -> Result<task::Model, DbErr> {
     let todo = task::Entity::find_by_id(task_id).one(db).await?;
     let mut todo: task::ActiveModel = todo.unwrap().into();
     todo.is_done = Set(if is_done { 1 } else { 0 });
-    let todo = todo.update(db).await?;
-    Ok(todo)
+    let todo = todo.update(db).await;
+    todo
+}
+
+pub async fn get_all_undone_tasks(db: &DbConn) -> Result<Vec<task::Model>, DbErr> {
+    let todos = task::Entity::find()
+        .filter(task::Column::IsDone.eq(0))
+        .all(db)
+        .await;
+    todos
 }
--- tests/test_entity_wrapper.rs
+++ tests/test_entity_wrapper.rs
@@ -3,7 +3,7 @@ use common::get_db_conn;
 
 use entity::task;
 use migration::DbErr;
-use todo_rs::{create_task, update_task_is_done};
+use todo_rs::{create_task, get_all_undone_tasks, update_task_is_done};
 
 use chrono::{Local, SubsecRound};
 use sea_orm::{ActiveModelTrait, EntityTrait, PaginatorTrait, Set};
@@ -48,6 +48,33 @@ async fn test_update_task_is_done() -> Result<(), DbErr> {
     // then
     assert_eq!(todo.title, task_title);
     assert_eq!(todo.is_done, 1);
+    let count = task::Entity::find().count(&db).await.unwrap_or(0);
+    assert_eq!(1, count);
 
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_get_all_undone_tasks() -> Result<(), DbErr> {
+    // given we have created three tasks, of which one is marked done
+    let db = get_db_conn().await;
+    for i in 0..=2 {
+        let todo = task::ActiveModel {
+            id: Set(Uuid::new_v4().to_owned().to_string()),
+            title: Set(format!("Task title 00{}", i)),
+            created_at: Set(Local::now()
+                .round_subsecs(0)
+                .format("%F %H:%M:%S")
+                .to_string()),
+            is_done: Set(if i == 0 { 1 } else { 0 }),
+            ..Default::default()
+        };
+        let _todo = todo.insert(&db).await;
+    }
+    // when I fetch all the undone takss
+    let all_undone_tasks: Vec<task::Model> = get_all_undone_tasks(&db).await?;
+
+    // then I should only get two tasks whose status is undone
+    assert_eq!(all_undone_tasks.len(), 2);
     Ok(())
 }
